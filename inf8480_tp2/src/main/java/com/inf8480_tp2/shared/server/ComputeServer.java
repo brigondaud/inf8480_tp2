ceb193a424b8ca0bf38f28606054fe37130db422
Loic Poncet <loicponcet05@gmail.com>
2018-11-02 14:49:21 -0400

    Introduced the possibility for a ComputeServer to be corrupted

--- inf8480_tp2/src/main/java/com/inf8480_tp2/shared/server/ComputeServer.java
+++ inf8480_tp2/src/main/java/com/inf8480_tp2/shared/server/ComputeServer.java
@@ -13,12 +13,15 @@ import java.rmi.RemoteException;
 public interface ComputeServer extends Remote {
 
     /**
-     * Execute an operation on the server
+     * Execute an operation on the server. If the server is corrupted, then a random floating point number is
+     * generated and if this number is between 0 and the corruption rate of the server, then a false result will be
+     * returned.
      *
      * @param operation The operation to execute
      * @param login The login of the dispatcher asking to execute the task
      * @param password The password of the dispatcher asking to execute the task
-     * @return A Response object
+     * @return A Response object containing the result of the operation, or eventually a false result
+     * if the server is corrupted. If the server refused the task, then an OutOfCapacityResponse is returned.
      */
     Response executeOperation(Operation operation, String login, String password) throws RemoteException;
 
