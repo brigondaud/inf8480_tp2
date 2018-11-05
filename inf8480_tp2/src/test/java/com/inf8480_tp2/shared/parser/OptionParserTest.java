520152aeb9331be69c9e5962b052f1d8877c2d2a
Baptiste <baptiste.rigondaud@ensimag.fr>
2018-11-05 18:50:04 -0500

    Corrected test

--- inf8480_tp2/src/test/java/com/inf8480_tp2/shared/parser/OptionParserTest.java
+++ inf8480_tp2/src/test/java/com/inf8480_tp2/shared/parser/OptionParserTest.java
@@ -11,7 +11,7 @@ public class OptionParserTest {
     @Test
     public void corruptOptionTest() {
         String[] notCorrupted = {"--portDir", "5001"};
-        String[] corrupted = {"--unsafe", "--corrupt", "75.5"};
+        String[] corrupted = {"--unsafe", "--corrupt", "75"};
         String[] corruptOptionOnly = {"--corrupt", "50"};
         OptionParser parser = new OptionParser(notCorrupted);
         assertTrue(parser.isSafeMode());
